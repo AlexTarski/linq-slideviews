c7195accccb69919b9a8180ea3a8ffba777a6706
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-06-02 20:20:34 +0300

    Add ParseSlideRecords() and ParseVisitRecords() methods that get information about slides and visits of slide from input lines

--- ParsingTask.cs
+++ ParsingTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 
 namespace linq_slideviews;
@@ -11,7 +12,12 @@ public class ParsingTask
 	/// <remarks>Метод должен пропускать некорректные строки, игнорируя их</remarks>
 	public static IDictionary<int, SlideRecord> ParseSlideRecords(IEnumerable<string> lines)
 	{
-		throw new NotImplementedException();
+		IDictionary<int, SlideRecord> result = lines.Select(line => line.Split(';'))
+			.Where(line => line.Length == 3 
+						&& Enum.TryParse(line[1], true, out SlideType slideType)
+						&& int.TryParse(line[0], out int slideID))
+			.ToDictionary(line => int.Parse(line[0]), line => new SlideRecord(int.Parse(line[0]), Enum.Parse<SlideType>(line[1], true), line[2]));
+		return result;
 	}
 
 	/// <param name="lines">все строки файла, которые нужно распарсить. Первая строка — заголовочная.</param>
@@ -22,6 +28,21 @@ public class ParsingTask
 	public static IEnumerable<VisitRecord> ParseVisitRecords(
 		IEnumerable<string> lines, IDictionary<int, SlideRecord> slides)
 	{
-		throw new NotImplementedException();
+		return lines.Select(line => { string[] splitedLine = line.Split(';'); if (splitedLine.Length != 4) { throw new FormatException($"Wrong line [{line}]"); } return splitedLine; })
+			.Skip(1)
+			//.Where(line => line.Length > 0 && DateTime.TryParseExact($"{line[2]} {line[3]}", "YYYY-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateTime))
+			.Where(line => line.Length > 0)
+			.Select(line =>
+			{
+				if (DateTime.TryParseExact($"{line[2]} {line[3]}", "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dateTime)
+					&& int.TryParse(line[0], out int userID) && int.TryParse(line[1], out int slideID) && slides.ContainsKey(slideID))
+				{
+					return new VisitRecord(userID, slideID, dateTime, slides[slideID].SlideType);
+				}
+				else
+				{
+					throw new FormatException($"Wrong line [{line[0]};{line[1]};{line[2]};{line[3]}]");
+				}
+			});
 	}
 }
